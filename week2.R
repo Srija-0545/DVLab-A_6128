69ca8030c0e127882f51b01650f809bd13064d6a
Your Name <you@example.com>
2026-02-27 12:28:54 +0530

    tdgfh

--- week2.R
+++ week2.R
@@ -1,55 +1,69 @@
-#Load Air passengers Data set 
-data(AirPassengers)
+# Load Dataset
+data("iris")
 
+# Inspect the Data
+?iris
+head(iris, 3)
+str(iris)
+class(iris)
 
-#verify the dataset
-?AirPassengers
-class(AirPassengers)
-View(AirPassengers)
+# very Basic Bar Chart
+# Count Of Each Species
+# To see the Data in the column "Species"
+iris$Species
+View(iris)
 
-#Convert the dataset to dataframe
-ap_df <- data.frame(
-  year <- time(AirPassengers),
-  passengers = as.numeric(AirPassengers)
+# plot iris
+# plotting a bar chart
+barplot(
+  table(iris$Species)
 )
 
-# data Frame with year and months separately 
-ap_df_months <-data.frame(
-  year = floor(time(AirPassengers)),
-  month = cycle(time(AirPassengers)),
-  passengers = as.numeric(AirPassengers)
+# labeled bar chart
+barplot(
+  table(iris$Species),
+  main = "count of Iris Species",
+  xlab = "Species",
+  ylab = "Number of Items",
+  col = "steelblue"
 )
-ap_df_months
 
-#Basic Plot
-plot(AirPassengers)
+# Add title, axis label and color
+# Bar chart from Aggregate Data
+# Using Mean Sepal length per species
+mean_sepal <- tapply(iris$Sepal.Length, iris$Species, mean)
+mean_sepal
 
-# plot with title, Axis labels and color
-plot(AirPassengers,
-     type = 'l',
-     main ="Air Passengers Trend Analysis",
-     xlab = "Months",
-     ylab = "No.of. Passengers",
-     col= "red")
-points(AirPassengers,
-       type = 'o',
-       pch = 16,
-       col = "blue")
-grid()
-library(ggplot2)
+barplot(
+  mean_sepal,
+  col = "orange",
+  main = "Average Sepal Length By Species",
+  xlab = "Species",
+  ylab = "Mean Sepal Length"
+)
 
-ggplot(ap_df,
-       aes(x = year, y =passengers))+
-  geom_line(color='darkgreen', linewidth = 0.5)+
-  labs(
-    title = 'Trend Analysis os Air Passengers',
-    subtitle = 'From 1949-1960',
-    caption = 'Using Built-in Air Passengers Dataset',
-    x = 'Months',
-    y = 'No.of Passengers') +
-  geom_point(color = 'red', size=0.5)+
-  geom_smooth(se=FALSE,color ='orange') +
-  theme_minimal()+
-  theme(
-    plot.title = element_text(face = "bold", size = 14),
-    plot.subtitle = element_text(size = 10))
+# grouped bar chart
+# compare sepal and petal Length (Mean)
+group_means <- rbind(
+  sepal = tapply(iris$Sepal.Length, iris$Species, mean),
+  petal = tapply(iris$Petal.Length, iris$Species, mean)
+)
+group_means
+
+# Grouped (Clustered) Bar Chart
+barplot(
+  group_means,
+  beside = TRUE,
+  col = c("skyblue", "pink"),
+  legend.text = TRUE,
+  main = "Clustered Bar Chart: Sepal Vs Petal Length"
+)
+
+# Grouped (Stacked) Bar Chart
+barplot(
+  group_means,
+  beside = FALSE,
+  col = c("skyblue", "pink"),
+  legend.text = TRUE,
+  main = "Stacked Bar Chart: Sepal Vs Petal Length"
+)
